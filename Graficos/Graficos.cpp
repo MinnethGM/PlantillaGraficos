683c49eeba885b3477ec62a514e3017a11af221b
alumno <alumno@D3A08>
2018-01-22 10:49:42 -0700

    triangulo

--- Graficos/Graficos.cpp
+++ Graficos/Graficos.cpp
@@ -26,6 +26,19 @@ void actualizar() {
 }
 
 void dibujar() {
+	glBegin(GL_TRIANGLES);//Inicia la rutina con un modo de dibujo
+
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(-1.0f, 0.0f, 0.0f);
+
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(1.0f, 0.0f, 0.0f);
+
+
+	glEnd();//finaliza rutina
 
 }
 
